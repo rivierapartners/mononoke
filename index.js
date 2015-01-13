e6800419e8eb8ef10a4c88f95785e1de7b2d526e
Viktor Chynarov <vchynarov@rivierapartners.com>
2015-01-12 16:52:01 -0800

    Works with default empty globs

--- index.js
+++ index.js
@@ -1,13 +1,42 @@
 var watch = require('watch');
+var minimatch = require('minimatch');
+
+var checkGlobs = function (callback, globs, filename) {
+
+    if(typeof(callback) !== "function") {
+        return;
+    }
+
+    console.log("In checkglobs!");
+    console.log(callback);
+    console.log(globs);
+    console.log(filename);
+
+    if (globs.length) {
+        for(i in globs) {
+            if(globs.hasOwnProperty(i)) {
+                var currentGlob = globs[i];
+                if(minimatch(filename, currentGlob)) {
+                    callback(filename);
+                }
+            }
+        }
+    }
+    else {
+        callback(filename);
+    }
+}
 
 /*
-
+ This is the constructor for a tree watcher that creates an EvenEmitter and
+ watches a tree. This is accessed through TreeWatch.watch(root).
 */
-var _watcher = function(root) {
+var _watcher = function(root, globs) {
     var _createdCallback = null;
     var _changedCallback = null;
     var _self = this;
     this.root = root; 
+    if(typeof(globs) === "undefined") { console.log("undef!"); globs = [ ]; }
 
     this.created = function(callback) {
         _createdCallback = callback;
@@ -21,14 +50,10 @@ var _watcher = function(root) {
 
     watch.createMonitor(this.root, function(monitor) {
         monitor.on("created", function (f, stat) {
-            if ( typeof(_createdCallback === "function") ) {
-                _createdCallback(f);
-            }
+            checkGlobs(_createdCallback, globs, f);
         })
         monitor.on("changed", function (f, curr, prev) {
-            if ( typeof(this.changed === "function") ) {
-                _changedCallback(f);
-            }
+            checkGlobs(_changedCallback, globs, f);
         })
     })
 
