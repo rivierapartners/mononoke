920e51394d505409a0f3981082f5984558de9834
Viktor Chynarov <vchynarov@rivierapartners.com>
2015-01-13 16:28:43 -0800

    Organized specs

--- spec/mainSpec.js
+++ spec/mainSpec.js
@@ -10,22 +10,21 @@ var PROJECT_ROOT = process.cwd(); // If this is using `jasmine` or `npm test`
 
 jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
 
-describe("Top levelfiles with no globbing", function() {
-    var spy = null;
-    dirconf = {
-            'README.md' : 'blah',
-            'yo.txt' : 'textfile1',
-            'yoyoyo.txt' : 'textfile2',
-            'hello.scss' : 'SCSS',
-            'theme.scss' : 'yo',
-            'basic-theme.css' : 'yo'
-    };
+var dirconf = {
+    'README.md' : 'blah',
+    'yo.txt' : 'textfile1',
+    'yoyoyo.txt' : 'textfile2',
+    'hello.scss' : 'SCSS',
+    'theme.scss' : 'yo',
+    'basic-theme.css' : 'yo'
+};
 
+
+describe("mononoke filetree watching", function() {
     beforeEach(function(done) {
         setTimeout(function() {
             utils.init();
             utils.initTestDir(STYLESHEETS_DIR, dirconf );
-            spy = sinon.spy();
             done();
         }, 2000)
     });
@@ -34,19 +33,31 @@ describe("Top levelfiles with no globbing", function() {
         utils.wipe(PROJECT_ROOT);
     });
 
-    it("should recognize created files", function(done) {
-        var cwd = process.cwd();
-        mononoke.watch(cwd)
-            .created(spy);
+    describe("observing files in root with no globs", function() {
+        var spy = null;
+        beforeEach(function() {
+            spy = sinon.spy();
+        });
+        it("should recognize created files", function(done) {
+            var cwd = process.cwd();
+            mononoke.watch(cwd)
+                .created(spy);
 
-        var filepath = "whywy.txt";
-        var fullpath = path.join(cwd,filepath);
-        setTimeout(function() {
-            utils.createFile(STYLESHEETS_DIR, filepath, "blah");
-            utils.spyInspect(done, spy, fullpath);
-        }, 5000);
-
+            var filepath = "whywy.txt";
+            var fullpath = path.join(cwd,filepath);
+            setTimeout(function() {
+                utils.createFile(STYLESHEETS_DIR, filepath, "blah");
+                utils.spyInspect(done, spy, fullpath);
+            }, 5000);
+        });
     });
 });
 
+/*
+describe("Arbitrarily nested files with several filters", function() {
+    it("should dispath the correct callback to the glob-matched file", function(done) {
 
+
+    });
+})
+*/
