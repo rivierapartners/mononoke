1629f428f503ab399dd66f9b494ef46eb02e69f4
Viktor Chynarov <vchynarov@rivierapartners.com>
2015-01-14 17:02:20 -0800

    got it

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ var checkGlobs = function (callback, globs, filename) {
  This is the constructor for a tree watcher that creates an EvenEmitter and
  watches a tree. This is accessed through TreeWatch.watch(root).
 */
-var _watcher = function(root, globs) {
+var _watcher = function(watch, root, globs) {
     var _createdCallback = null;
     var _changedCallback = null;
     var _self = this;
@@ -44,6 +44,7 @@ var _watcher = function(root, globs) {
         return _self;
     }; 
 
+    console.log(watch);
     watch.createMonitor(root, function(monitor) {
         monitor.on("created", function (f, stat) {
             checkGlobs(_createdCallback, globs, f);
@@ -53,14 +54,21 @@ var _watcher = function(root, globs) {
         })
     })
 
+
     return _self;
 };
 
 
 var mononoke = {
+    "_watcher" : null,
     "watch" : function(root, globs) {
+        watch = this._watcher || watch; // For testing.
+
         if(typeof(globs) === "undefined") { globs = [ ]; }
-        return new _watcher(root, globs);
+        return new _watcher(watch, root, globs);
+    },
+    "setWatcher" : function(watcher) {
+        this._watcher = watcher;
     }
 };
 
--- spec/mainSpec.js
+++ spec/mainSpec.js
@@ -1,117 +1,68 @@
-var fs = require('fs.extra'); // Does this actually work?
+var rewire = require('rewire');
 var sinon = require('sinon');
-require('jasmine-sinon') // Add matchers for sinon.js
+require('jasmine-sinon');
+var watch = require('watch');
+var EventEmitter = require('events').EventEmitter;
 var path = require('path');
 var mononoke = require('../index.js');
-var utils = require('./utils.js');
+var assert = require('assert');
 
-var STYLESHEETS_DIR = "MononokeTesting/assets/vendor/sample/stylesheets";
-var RESET_TIME = 1000;
-var WATCH_TIME = 3500;
-var PROJECT_ROOT = process.cwd(); // If this is using `jasmine` or `npm test`
-var TEST_ROOT = null;
-jasmine.DEFAULT_TIMEOUT_INTERVAL = 6000;
+var FilenameEmitter = null;
+var original = watch.createMonitor; // Original createMonitor function to actually process things correctly.
 
-var dirconf = {
-    'README.md' : 'blah',
-    'yo.txt' : 'textfile1',
-    'yoyoyo.txt' : 'textfile2',
-    'hello.scss' : 'SCSS',
-    'theme.scss' : 'yo',
-    'basic-theme.css' : 'yo',
-    'almost-done.html' : 'sdfsdf'
-};
-
+var mockWatch = {
+    "createMonitor" : function(root, callback) {
+        var spy = function(originalMonitor) {
+            FilenameEmitter = originalMonitor; // Intercept the monitor to allow our own emits.
+            callback(originalMonitor);
+        };
+        original(root, spy);
+    },
+    "unwatchTree" : watch.unwatchTree
+}
 
 beforeAll(function() {
-    // Need to set the testing root correctly.
-    utils.init();
-    TEST_ROOT = process.cwd();
-    process.chdir(PROJECT_ROOT);
+    mononoke.setWatcher(mockWatch);
 });
 
-describe("mononoke filetree watching", function() {
-    var cwd = null;
+describe("maintest", function() {
+    var createdSpy = null;
+    var changedSpy = null;
 
-    beforeEach(function(done) {
-        cwd = process.cwd();
-        setTimeout(function() {
-            utils.init();
-            utils.initTestDir(STYLESHEETS_DIR, dirconf );
-            done();
-        }, RESET_TIME)
+    beforeEach(function() {
+        createdSpy = sinon.spy();
+        changedSpy = sinon.spy();
+        console.log("Before each test.");
     });
 
     afterEach(function() {
-        utils.wipe(PROJECT_ROOT);
+        console.log("Finishing test");
+        mockWatch.unwatchTree('./');
     });
 
-    describe("observing files in root with no globs", function() {
-        var spy = null;
+    describe("basic", function() {
         beforeEach(function() {
-            spy = sinon.spy();
-        });
-        it("should recognize created files", function(done) {
-            mononoke.watch(cwd)
-                .created(spy);
-
-            setTimeout(function() {
-                var filepath = "whywy.txt";
-                var fullpath = path.join(TEST_ROOT, STYLESHEETS_DIR,filepath);
-                utils.createFile(cwd, filepath, "blah");
-                utils.inspecter(fullpath, done, function() {
-                   expect(spy).toHaveBeenCalledOnce();
-                   expect(spy).toHaveBeenCalledWith(fullpath);
-                });
-            }, WATCH_TIME);
-        });
-    });
-
-    describe("Top level files with several filters", function() {
-        var createdSpy = null;
-        var changedSpy = null;
-
-        beforeEach(function() {
-            createdSpy = sinon.spy();
-            changedSpy = sinon.spy();
-        });
-
-        it("should dispath the correct callback to the glob-matched file", function(done) {
-            mononoke.watch(cwd, ['*.scss', '*.html'])
+            console.log("inner before test");
+            mononoke.watch('./') 
                 .created(createdSpy)
-                .changed(changedSpy)
-
-            // Add files
-            setTimeout(function() {
-                // NOTE: Creating and appending to the file right after is 
-                // pointless, since the watch scanner will only register
-                // the new file.
-                utils.createFile(cwd, "main_page.html", "o");
-                utils.createFile(cwd, "other_page.html", "o");
-                utils.createFile(cwd, "vendor_style.scss", "o");
-
-                utils.createFile(cwd, "unneeded.css", "o");
-                utils.createFile(cwd, "unneeded.htm", "o");
-                utils.createFile(cwd, "unneeded.txt", "o");
-                
-                utils.appendFile(cwd, "unneeded.htm", "lolololol ol");
-                utils.appendFile(cwd, "almost-done.html", "dfsdfdsf");
-                utils.appendFile(cwd, "hello.scss", "additional");
-
-                utils.inspecter(cwd, done, function() {
-                    expect(createdSpy).toHaveBeenCalledThrice();
-                    expect(createdSpy).toHaveBeenCalledWith(path.join(TEST_ROOT, STYLESHEETS_DIR, "main_page.html"));
-                    expect(createdSpy).toHaveBeenCalledWith(path.join(TEST_ROOT, STYLESHEETS_DIR, "other_page.html"));
-                    expect(createdSpy).toHaveBeenCalledWith(path.join(TEST_ROOT, STYLESHEETS_DIR, "vendor_style.scss"));
-
-                    expect(changedSpy).toHaveBeenCalledTwice();
-                    expect(changedSpy).toHaveBeenCalledWith(path.join(TEST_ROOT, STYLESHEETS_DIR, "almost-done.html"));
-                    expect(changedSpy).toHaveBeenCalledWith(path.join(TEST_ROOT, STYLESHEETS_DIR, "hello.scss"));
-                });
-            }, WATCH_TIME);
+                .changed(changedSpy);
         });
-    })
+
+        it("should work", function(done) {
+            setTimeout(function() {
+                console.log("FilenameEmitter is set...");
+                FilenameEmitter.emit('created', 'file1.html');
+                FilenameEmitter.emit('changed', 'file2.scss');
+                FilenameEmitter.emit('created', 'file3.log');
+                FilenameEmitter.emit('changed', 'file4.js');
+                FilenameEmitter.emit('created', 'file5.js');
+
+                expect(createdSpy).toHaveBeenCalledThrice();
+                expect(changedSpy).toHaveBeenCalledTwice();
+                done();
+            }, 2000);
+        });
+
+    });
+
 });
-
-
-
